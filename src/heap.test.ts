68795901a90a0f97af83f3c51b0978186703fe79
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2020-01-17 13:10:00 +0800

    Add tests. Add comparer.

---
+++ src/heap.test.ts
@@ -0,0 +1,43 @@
+import { Heap, maxHeapCompare } from "./heap";
+
+test("can insert and pop", () => {
+  const heap = new Heap();
+  expect(heap.size).toBe(0);
+
+  heap.insert(0);
+  expect(heap.size).toBe(1);
+  expect(heap.pop()).toBe(0);
+  expect(heap.size).toBe(0);
+  expect(heap.pop()).toBe(null);
+  expect(heap.size).toBe(0);
+});
+
+test("can sort all its input by pop order", () => {
+  const input = [3, 1, 2, 65, 3, 2, 5, 8, -1, 0];
+  const expected = [-1, 0, 1, 2, 2, 3, 3, 5, 8, 65];
+
+  const heap = new Heap();
+  for (const x of input) {
+    heap.insert(x);
+  }
+
+  for (const x of expected) {
+    expect(heap.pop()).toBe(x);
+  }
+  expect(heap.size).toBe(0);
+});
+
+test("can specify comparer to adjust pop order", () => {
+  const input = [3, 1, 2, 65, 3, 2, 5, 8, -1, 0];
+  const expected = [65, 8, 5, 3, 3, 2, 2, 1, 0, -1];
+
+  const heap = new Heap(maxHeapCompare);
+  for (const x of input) {
+    heap.insert(x);
+  }
+
+  for (const x of expected) {
+    expect(heap.pop()).toBe(x);
+  }
+  expect(heap.size).toBe(0);
+});
---
+++ src/heap.ts
@@ -0,0 +1,97 @@
+export function minHeapCompare<T>(a: T, b: T): number {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+}
+
+export function maxHeapCompare<T>(a: T, b: T): number {
+  return -minHeapCompare(a, b);
+}
+
+/** Implements a generic min-heap! */
+export class Heap<T> {
+  storage: Array<T>;
+  comparer: (a: T, b: T) => number;
+
+  constructor(comparer?: (a: T, b: T) => number) {
+    this.comparer = comparer || minHeapCompare;
+    this.storage = [];
+  }
+
+  /** Returns the size of the heap. */
+  get size() {
+    return this.storage.length;
+  }
+
+  /** Inserts a new value to the heap. */
+  insert(value: T) {
+    this.storage.push(value);
+    this.resolve(this.storage.length - 1);
+  }
+
+  /** Removes and returns the smallest value from the heap. */
+  pop(): T | null {
+    // Early exit if heap is empty!
+    if (this.storage.length === 0) {
+      return null;
+    }
+
+    // Store the smallest value in the heap for return later.
+    const value = this.storage[0];
+
+    // Move the last value in the heap up to the top.
+    this.storage[0] = this.storage[this.storage.length - 1];
+    this.storage = this.storage.slice(0, -1);
+
+    // From root to top, arrange the heap! This operation is O(log N).
+    let index = 0;
+    while (index < this.storage.length) {
+      const left = index * 2 + 1;
+      const right = index * 2 + 2;
+
+      // Survey which child index we should swap the current index with.
+      let minIdx = index;
+      if (
+        left < this.storage.length &&
+        this.comparer(this.storage[minIdx], this.storage[left]) >= 0
+      ) {
+        minIdx = left;
+      }
+      if (
+        right < this.storage.length &&
+        this.comparer(this.storage[minIdx], this.storage[right]) >= 0
+      ) {
+        minIdx = right;
+      }
+
+      // If we haven't found a swap, we break since heap is sorted at this point.
+      if (minIdx === index) {
+        break;
+      }
+
+      // Else, we do a swap and iterate!
+      this.swap(index, minIdx);
+      index = minIdx;
+    }
+
+    return value;
+  }
+
+  /** Resolve heap placing from children to root. This operation is O(log N). */
+  private resolve(index: number) {
+    while (index > 0) {
+      const parent = Math.floor((index - 1) / 2);
+      if (this.comparer(this.storage[index], this.storage[parent]) < 0) {
+        this.swap(index, parent);
+      }
+      index = parent;
+    }
+  }
+
+  /** Swaps values from two indices in the heap. */
+  private swap(i: number, j: number) {
+    const temp = this.storage[i];
+    this.storage[i] = this.storage[j];
+    this.storage[j] = temp;
+  }
+}
